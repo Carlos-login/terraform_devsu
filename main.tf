5879ab7fef95b248eb2050b034e5589f75c225fe
Carlos-login <163325301+Carlos-login@users.noreply.github.com>
2024-07-20 13:47:21 +0000

    agregar ecr

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
 }
+
+resource "aws_ecr_repository" "my_ecr" {
+  name                 = "devsu"
+// escaneo de vulne
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}

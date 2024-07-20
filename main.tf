43abc0c77fd5b302dde625b097c91104aa8999b0
Carlos-login <163325301+Carlos-login@users.noreply.github.com>
2024-07-20 15:26:57 +0000

    fix

--- main.tf
+++ main.tf
@@ -7,13 +7,13 @@ resource "aws_vpc" "devsu_vpc" {
 resource "aws_subnet" "devsu_subnet_public1" {
   vpc_id     = aws_vpc.devsu_vpc.id
   cidr_block = "10.0.0.0/24"
-
+  availability_zone = "us-east-1a"
 }
 
 resource "aws_subnet" "devsu_subnet_public2" {
   vpc_id     = aws_vpc.devsu_vpc.id
   cidr_block = "10.0.1.0/24"
-
+  availability_zone = "us-east-1b"
 }
 
 resource "aws_internet_gateway" "devsu_gw" {

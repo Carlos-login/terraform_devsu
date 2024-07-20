03d71b3f4b40dea639aeb02449b11fea4a5684de
Carlos-login <163325301+Carlos-login@users.noreply.github.com>
2024-07-20 14:30:31 +0000

    add public routers

--- main.tf
+++ main.tf
@@ -16,6 +16,33 @@ resource "aws_subnet" "devsu_subnet_public2" {
 
 }
 
+resource "aws_internet_gateway" "devsu_gw" {
+  vpc_id = aws_vpc.devsu_vpc.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.devsu_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.devsu_gw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.devsu_subnet_public1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.devsu_subnet_public2.id
+  route_table_id = aws_route_table.public.id
+}
+
 resource "aws_ecr_repository" "my_ecr" {
   name                 = "devsu"
 // escaneo de vulne

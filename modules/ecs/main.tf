2d51be01c4df655901b1c0d4a4f56955f1086dc9
Carlos-login <163325301+Carlos-login@users.noreply.github.com>
2024-07-21 23:31:18 +0000

    fix

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -25,7 +25,12 @@ resource "aws_security_group" "ecs_service_alb" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
